bcc1ed19fa3b27dc449dd96ca7a4d8c1f7a64c21
programanime <programanimne@gmail.com>
2019-10-11 15:04:09 -0500

    token

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 # cors = CORS(app,  origins=['http://localhost:8080'])
 cors = CORS(app)
 api = Api(app)
-
+idToken="123"
 from flask import jsonify
 from flask_restful import Resource,reqparse
 
@@ -24,17 +24,20 @@ parser = reqparse.RequestParser()
 @api.resource('/categories')
 class Categories(Resource):
     def get(self):
+        if request.headers.get('idToken') and  request.headers.get('idToken') != idToken:return jsonify({"result":"Token no valido", "ok":False})
         return jsonify({"status":"OK", "data":CategoryController.get_all()})
 
 @api.resource('/brands')
 class Brands(Resource):
     def get(self):
+        if request.headers.get('idToken') and  request.headers.get('idToken') != idToken:return jsonify({"result":"Token no valido", "ok":False})
         return jsonify({"brands":BrandController.get_all()})
     
 
 @api.resource('/items/category/<category_name>')
 class ProductsByCategory(Resource):
     def get(self,  category_name:str):
+        if request.headers.get('idToken') and  request.headers.get('idToken') != idToken:return jsonify({"result":"Token no valido", "ok":False})
         return jsonify({"products":ProductController.get_by_category(category_name)})
         
         # return jsonify({"products":listaItems})
@@ -42,6 +45,7 @@ class ProductsByCategory(Resource):
 @api.resource('/items/brand/<brand_name>')
 class ProductsByBrand(Resource):
     def get(self, brand_name:str):
+        if request.headers.get('idToken') and  request.headers.get('idToken') != idToken:return jsonify({"result":"Token no valido", "ok":False})
         items=ProductController.get_by_brand(brand_name)
         return jsonify({"products":items})
         
@@ -49,24 +53,28 @@ class ProductsByBrand(Resource):
 @api.resource('/items/<item_id>')
 class Product(Resource):
     def get(self, item_id):
+        if request.headers.get('idToken') and  request.headers.get('idToken') != idToken:return jsonify({"result":"Token no valido", "ok":False})
         item = ProductController.get(str(item_id))
         return jsonify(item)
 
 @api.resource('/search')
 class Products(Resource):
     def get(self):
+        if request.headers.get('idToken') and  request.headers.get('idToken') != idToken:return jsonify({"result":"Token no valido", "ok":False})
         nombre = request.args.get('q')
         return jsonify({"products":ProductController.get_by_name(nombre)})
 
 @api.resource('/checkout')
 class Checkouts(Resource):
     def post(self):
+        if request.headers.get('idToken') and  request.headers.get('idToken') != idToken:return jsonify({"result":"Token no valido", "ok":False})
         CheckoutController.save(request.json)
         return jsonify({"ok":True})
 
 @api.resource('/user/<user_name>/orders')
 class Order(Resource):
     def get(self, user_name:str):
+        if request.headers.get('idToken') and  request.headers.get('idToken') != idToken:return jsonify({"result":"Token no valido", "ok":False})
         items=CheckoutController.get_by_user(user_name)
         return jsonify({"orders":items})
 
@@ -76,8 +84,8 @@ class Order(Resource):
 @api.resource("/")
 class AllProducts(Resource):
     def get(self):
+        if request.headers.get('idToken') and  request.headers.get('idToken') != idToken:return jsonify({"result":"Token no valido", "ok":False})
         items = ProductController.get_all()
         return jsonify(items)
-
 
 app.run(host= '0.0.0.0', port=environ.get('PORT'))
